dde53e42496d75314f0bc6213b74a06b10e93031
LeeDahee <dduosy9@gmail.com>
2023-08-30 12:41:32 +0900

    join - 비밀번호 유효성 검사, 전체 검사 기능

--- 4_finalProject/assets/js/join.js
+++ 4_finalProject/assets/js/join.js
@@ -1,34 +1,98 @@
 'use strict';
 
-const userArr = [
-  {
-    id: 'abc',
-    password: '123546',
-  },
-  {
-    id: 'bca',
-    password: '123456a',
-  },
-];
+const userArr = [];
 
+let isAvailable = false;
+let isCheckedDup = false;
 const joinBtn = document.querySelector('.footer__submit');
 const joinForm = document.querySelector('.form-join');
 const dupButton = document.querySelector('.btn-check-id');
+const pw_input = document.querySelector('.password__input');
 
-// 중복 검사(id)
+// 전체 함수
+function validation(e) {
+  e.preventDefault();
 
-// 필수 입력 검사
+  const allEnterCheck = isEnteredAll();
+  const duplicateCheck = isDuplicate();
+  const passwordCheck = isValidPassword();
+
+  const id_value = document.querySelector('.id__input').value;
+  const pw_value = document.querySelector('.password__input').value;
+  const phone_value = document.querySelector('.phone__input').value;
+
+  console.log(allEnterCheck, isCheckedDup, duplicateCheck, passwordCheck);
+  if (!allEnterCheck) {
+    alert('필수 항목을 모두 입력해주세요.');
+  } else if (
+    allEnterCheck &&
+    isCheckedDup &&
+    !duplicateCheck &&
+    passwordCheck
+  ) {
+    addUser(id_value, pw_value, phone_value);
+    console.log(userArr);
+    alert('회원가입 완료');
+  }
+}
+
+function addUser(id, pw, phone) {
+  userArr.push({ id, pw, phone });
+}
+
+// 중복 검사(id) => 완성
+function isDuplicate() {
+  const id_input = document.querySelector('.id__input');
+  const id_value = document.querySelector('.id__input').value;
+  const id_des = document.querySelector('.id__validation-description');
+  let isDup = false;
+
+  if (isNull(id_value)) {
+    id_des.textContent = '아이디를 입력하세요';
+    id_des.style.color = '#d34141';
+  } else {
+    userArr.forEach((user) => {
+      if (user.id === id_value) {
+        isDup = true;
+      }
+    });
+
+    if (!isDup) {
+      isCheckedDup = true;
+      id_input.setAttribute('disabled', true);
+      dupButton.setAttribute('disabled', true);
+      id_input.classList.add('disabled');
+      dupButton.classList.add('disabled');
+    }
+
+    return isDup;
+  }
+}
+
+function paintDuplicateCheck(check) {
+  const id_des = document.querySelector('.id__validation-description');
+  console.log(check);
+  if (check) {
+    id_des.textContent = '이미 사용중인 아이디입니다.';
+    id_des.style.color = '#d34141';
+  } else {
+    id_des.textContent = '사용 가능한 아이디입니다.';
+    id_des.style.color = '#1baa1b';
+  }
+}
+
+// 필수 입력 검사 => 완성
 function isEnteredAll() {
   const id = document.querySelector('.id__input').value;
   const password = document.querySelector('.password__input').value;
   const phone = document.querySelector('.phone__input').value;
 
   if (isNull(id) || isNull(password) || isNull(phone)) {
-    console.log(false);
-    // return false;
+    // console.log(false);
+    return false;
   } else {
-    console.log(true);
-    // return true;
+    // console.log(true);
+    return true;
   }
 }
 function isNull(str) {
@@ -37,19 +101,45 @@ function isNull(str) {
   } else return false;
 }
 
-function canJoin(e) {
-  e.preventDefault();
+// 유효성 검사(password, 영문, 숫자, 특수문자의 조합으로 8자리 이상)
+function isValidPassword() {
+  const password = document.querySelector('.password__input').value;
+  const passwordRule =
+    /^[A-Za-z0-9`\-=\\\[\];',\./~!@#\$%\^&\*\(\)_\+|\{\}:"<>\?]{8,16}$/;
+  let validPasswordResult = true;
 
-  const check = isEnteredAll();
-  if (check) {
-    alert('회원가입 완료');
+  if (password.length < 8) {
+    validPasswordResult = false;
+  }
+
+  if (!passwordRule.test(password)) {
+    validPasswordResult = false;
+  }
+  // console.log(validPasswordResult);
+  return validPasswordResult;
+}
+
+function paintPasswordCheck(check) {
+  const password_des = document.querySelector(
+    '.password__validation-description'
+  );
+
+  if (!check) {
+    password_des.textContent = '8~16자 영문, 숫자, 특수문자를 사용하세요.';
+    password_des.style.color = '#d34141';
   } else {
-    alert('필수 항목을 모두 입력해주세요.');
+    password_des.textContent = '';
   }
 }
 
-joinForm.addEventListener('submit', canJoin);
-
-// 유효성 검사(password)
-
 // 유효성 검사(phone)
+
+joinForm.addEventListener('submit', validation);
+dupButton.addEventListener('click', () => {
+  const isDup = isDuplicate();
+  paintDuplicateCheck(isDup);
+});
+joinBtn.addEventListener('click', () => {
+  const isValidPW = isValidPassword();
+  paintPasswordCheck(isValidPW);
+});
